f981c3287e61c685d3e3818215fa02ae604c6b7e
Nour <nbenmoulehom@gmail.com>
2024-09-30 10:56:30 +0100

    prosit 3 done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,46 @@
+package src;
+
+import src.Animal;
+import src.Zoo;
+
+import static src.Zoo.compareZoos;
+
 public class Main {
 
 
     public static void main(String[] args) {
         Animal lion = new Animal("9tates", "Lion", 5, true);
 
-        Zoo zoo = new Zoo("Belvedere", "Tunis", 40);
+        Zoo zoo = new Zoo("Belvedere", "Tunis");
 
-        zoo.animals[0] = lion;
+        boolean isAdded = zoo.addAnimal(lion);
+        boolean isAdded2 = zoo.addAnimal(new Animal("3safer", "baba8ayou", 3, false));
+
+        zoo.displayAnimals();
+        /*System.out.println(isAdded);
+
+        System.out.println(zoo.toString());*/
+
+        System.out.println("Animal lion exist at: " + zoo.searchAnimal(lion));
+
+        System.out.println("Animal removing lion operation is:" + zoo.removeAnimal(lion));
+
+        System.out.println("Animal lion exist at: " + zoo.searchAnimal(lion));
+
+        zoo.displayAnimals();
+
 
         System.out.println(zoo.toString());
+
+        Zoo zoo2 = new Zoo("AfricaPark", "Sousse");
+        zoo2.addAnimal(lion);
+        zoo2.addAnimal(new Animal("7out", "dauphin", 1, true));
+        zoo2.addAnimal(new Animal("idk", "zarafa", 2, true));
+
+        Zoo BiggestZoo = compareZoos(zoo, zoo2);
+
+        System.out.println("\uD83E\uDE98 And the biggest zoo is *drums* :" + BiggestZoo.toString());
+
+        System.out.println("Testing isZooFull: " + zoo.isZooFull());
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,25 +1,99 @@
+package src;
+
+import src.Animal;
+
 public class Zoo {
-    Animal[] animals;
+    static final int NUMBER_CAGES = 25;
+    public Animal[] animals;
     String name;
     String city;
-    int nbrCages;
+    int nbrAnimals;
 
-    public Zoo(String name, String city, int nbrCages) {
+
+    public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
         this.animals = new Animal[25];
+        this.nbrAnimals = 0;
+    }
+
+    public Zoo () {
+
+    }
+
+    public boolean addAnimal(Animal animal) {
+        if(searchAnimal(animal) == -1) {
+            // Animals doesn't exist and can be added
+            for(int i = 0; i < animals.length; i++) {
+                if(animals[i] == null) {
+                    animals[i] = animal;
+                    nbrAnimals++;
+                    return true;
+                }
+            }
+            System.out.println("Animals est saturee");
+            return false;
+        }  else {
+            System.out.println("Animal existe deja");
+            return false;
+        }
+
+
+    }
+
+    public void displayAnimals() {
+        System.out.println("Animals:");
+        for (int i = 0; i < animals.length; i++) {
+
+            if (animals[i] != null) {
+                System.out.println("Animal numero " + (i + 1) + " " + animals[i].toString());
+            }
+        }
+        System.out.println("****** FIN ANIMALS *****");
+    }
+
+    public int searchAnimal(Animal animal) {
+        for (int i = 0; i < animals.length; i++) {
+            if( (animals[i] != null) && (animals[i].name.equals(animal.name))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean removeAnimal(Animal animal) {
+        int index = searchAnimal(animal);
+        if(index != -1) {
+            animals[index] = null;
+            nbrAnimals--;
+            return true;
+        } else {
+            // Animal doesn't exist
+            return false;
+        }
+
+    }
+
+    boolean isZooFull() {
+        return nbrAnimals == NUMBER_CAGES;
+    }
+
+    static Zoo compareZoos(Zoo zoo1, Zoo zoo2) {
+        if(zoo1.nbrAnimals > zoo2.nbrAnimals) {
+            return zoo1;
+        }
+        return zoo2;
     }
 
     @Override
     public String toString() {
-        String result = "Zoo: " + name + " in " + city + " has " + nbrCages + " cages.\n" + "Animals:\n";
 
-        for (int i = 0; i < animals.length; i++) {
-            if (animals[i] != null) {
-                result += animals[i].toString() + "\n";
-            }
-        }
-        return result;
+//        for (int i = 0; i < animals.length; i++) {
+//            if (animals[i] != null) {
+//                result += animals[i].toString() + "\n";
+//            }
+//        }
+        return "Zoo: " + name + " in " + city + " has " + NUMBER_CAGES + " cages," + " and number of animals in total: " + nbrAnimals;
     }
+
 }
